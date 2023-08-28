68c651b41ebc8d577bbc4b5dfd82b2bf153cc400
Roberth <45379245+roberth1927@users.noreply.github.com>
2023-08-28 15:44:17 -0500

    update

--- src/controllers/users/users.ts
+++ src/controllers/users/users.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express";
 import { handleHttpError, handleHttpSuccess } from "../../utils/httpHandle";
 import { registerNewUser, getUsers, uploadSignature  } from "../../services/user";
 
-
 const userPost = async ({ body }: Request, res: Response) => {
   const { name, email, phone_number, birthdate, signature } = body;
   await registerNewUser({ name, email, phone_number, birthdate, signature }, res);
@@ -27,38 +26,6 @@ const uploadImg = async (req: Request, res: Response) => {
     
   }
 
-
 };
 
-/*
-const userGet = async (req: Request, res: Response) => {
-  try {
-    const responseUsers = await getUser(req);
-    res.send(responseUsers);
-  } catch (e) {
-    handleHttpError(res, "ERROR_GET_USER");
-  }
-};
-
-const usersPut = async (
-  { params, body }: Request,
-  res: Response
-): Promise<void> => {
-  try {
-    const { id } = params;
-    const response = await updateUser(id, body);
-    res.send(response);
-  } catch (e) {}
-};
-
-const usersDelete = async (
-  { params, body }: Request,
-  res: Response
-): Promise<void> => {
-  const { id } = params;
-  const response = await deleteUser(id, body);
-  res.send(response);
-};
-
-export { usersGet, userPost, userGet, usersPut, usersDelete }; */
 export { userPost, usersGet, uploadImg };
--- src/models/server.ts
+++ src/models/server.ts
@@ -1,46 +1,46 @@
 
 import express, { Application } from 'express';
 import cors from 'cors';
-import connectToMongoDB from '../database/mongodb'; 
+import connectToMongoDB from '../database/mongodb';
 import { router } from "../routes"
 
 
 class Server {
-    private app: Application;
-    private port: number;
+  private app: Application;
+  private port: number;
 
-    constructor() {
-        this.app = express();
-        this.port = Number(process.env.PORT);
-        this.conectionDB();
-        this.middlewares();
-        this.routes();
+  constructor() {
+    this.app = express();
+    this.port = Number(process.env.PORT);
+    this.conectionDB();
+    this.middlewares();
+    this.routes();
 
-    }
+  }
 
-    async conectionDB() {
-        await connectToMongoDB();
-    }
-    private routes() {
-        this.app.use(router);
-    }
+  async conectionDB() {
+    await connectToMongoDB();
+  }
+  private routes() {
+    this.app.use(router);
+  }
 
-    private middlewares() {
-        this.app.use(cors({
-            origin: process.env.frontendDomain,
-            methods: ['GET', 'POST', 'PUT', 'DELETE'],
-            allowedHeaders: ['Content-Type', 'Authorization'],
-            exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
-        }));
-        this.app.use(express.json());
-    }
+  private middlewares() {
+    this.app.use(cors({
+      origin: '*', // Puedes ajustar esto a los orígenes permitidos en tu aplicación
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true,
+      exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
+    }));
+    this.app.use(express.json());
+  }
 
-
-    public listen() {
-        this.app.listen(this.port, () => {
-            console.log('Port', this.port);
-        });
-    }
+  public listen() {
+    this.app.listen(this.port, () => {
+      console.log('Port', this.port);
+    });
+  }
 }
 
 export default Server;
